250b89e464e6f3a8793ceda0fdd5254f34f93fcc
maxicatriel <maxicatriel@gmail.com>
2023-11-23 17:58:57 -0300

    correcion js

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -1,7 +1,7 @@
-console.log(location.search); // Lee los argumentos pasados a este formulario
-var id = location.search.substr(4); // producto_update.html?id=1
-console.log(id);
-const { createApp } = Vue;
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id = location.search.substr(4)  // producto_update.html?id=1
+console.log(id)
+const { createApp } = Vue
 createApp({
     data() {
         return {
@@ -12,26 +12,25 @@ createApp({
             stock: 0,
             precio: 0,
             url: 'https://maximilianovm.pythonanywhere.com/productos/' + id,
-           
-        };
+        }
     },
     methods: {
         fetchData(url) {
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
-                    this.id = data.id;
+                    console.log(data)
+                    this.id = data.id
                     this.nombre = data.nombre;
                     this.descripcion = data.descripcion;
-                    this.imagen = data.imagen;
-                    this.stock = data.stock;
-                    this.precio = data.precio;
+                    this.imagen = data.imagen
+                    this.stock = data.stock
+                    this.precio = data.precio
                 })
                 .catch(err => {
                     console.error(err);
-                    this.error = true;
-                });
+                    this.error = true
+                })
         },
         modificar() {
             let producto = {
@@ -40,29 +39,26 @@ createApp({
                 precio: this.precio,
                 stock: this.stock,
                 imagen: this.imagen
-            };
+            }
             var options = {
                 body: JSON.stringify(producto),
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 redirect: 'follow'
-            };
+            }
             fetch(this.url, options)
-                .then(() => {
-                    alert("Registro modificado");
-                    window.location.href = "./productos.html"; // Navega a productos.html
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./productos.html"; // navega a productos.html          
                 })
                 .catch(err => {
                     console.error(err);
-                    alert("Error al modificar");
-                });
-        },
-        regresar() {
-            window.location.href = "./productos.html";
-        },
-                      
-        created() {
-            this.fetchData(this.url);
+                    alert("Error al Modificar")
+                })
         }
-    }
-}).mount('#app');
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+}).mount('#app')
+
