bd7ddfaa945770a561c3b7160bd79a455cb4e3fc
maxicatriel <maxicatriel@gmail.com>
2023-11-23 16:01:52 -0300

    httpS

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -11,7 +11,7 @@ createApp({
             imagen: "",
             stock: 0,
             precio: 0,
-            url: 'http://maximilianovm.pythonanywhere.com/productos/' + id,
+            url: 'https://maximilianovm.pythonanywhere.com/productos/' + id,
             producto: [],
             carrito: [],
 
