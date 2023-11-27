90fcdafb6e847588d5c7bae31d9f8b9d8dde13e6
maxicatriel <maxicatriel@gmail.com>
2023-11-27 17:36:10 -0300

    uso Bcruypt

--- controladores/usuarios_controlador.py
+++ controladores/usuarios_controlador.py
@@ -2,26 +2,6 @@ from app import app, db
 from flask import jsonify, request
 from modelos.usuarios_modelo import Usuario, usuario_schema, usuarios_schema
 
-@app.route('/usuarios', methods=['GET'])
-def get_usuarios():
-    try:
-        all_usuarios = Usuario.query.all()
-        result = usuarios_schema.dump(all_usuarios)
-        return jsonify(result), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route('/usuarios/<id>', methods=['GET'])
-def get_usuario(id):
-    try:
-        usuario = Usuario.query.get(id)
-        if usuario:
-            return usuario_schema.jsonify(usuario), 200
-        else:
-            return jsonify({"message": "Usuario no encontrado"}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 @app.route('/usuarios', methods=['POST'])
 def create_usuario():
     try:
@@ -29,7 +9,9 @@ def create_usuario():
         contraseña = request.json['contraseña']
         mail = request.json['mail']
 
-        new_usuario = Usuario(nombreDeUsuario=nombreDeUsuario, contraseña=contraseña, mail=mail)
+        new_usuario = Usuario(nombreDeUsuario=nombreDeUsuario, mail=mail)
+        new_usuario.set_password(contraseña)
+
         db.session.add(new_usuario)
         db.session.commit()
 
@@ -43,22 +25,13 @@ def update_usuario(id):
         usuario = Usuario.query.get(id)
         if usuario:
             usuario.nombreDeUsuario = request.json['nombreDeUsuario']
-            usuario.contraseña = request.json['contraseña']
             usuario.mail = request.json['mail']
 
-            db.session.commit()
-            return usuario_schema.jsonify(usuario), 200
-        else:
-            return jsonify({"message": "Usuario no encontrado"}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+            # Actualiza la contraseña solo si se proporciona una nueva
+            nueva_contraseña = request.json.get('contraseña')
+            if nueva_contraseña:
+                usuario.set_password(nueva_contraseña)
 
-@app.route('/usuarios/<id>', methods=['DELETE'])
-def delete_usuario(id):
-    try:
-        usuario = Usuario.query.get(id)
-        if usuario:
-            db.session.delete(usuario)
             db.session.commit()
             return usuario_schema.jsonify(usuario), 200
         else:
--- modelos/usuarios_modelo.py
+++ modelos/usuarios_modelo.py
@@ -1,14 +1,23 @@
 from app import db, ma
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombreDeUsuario = db.Column(db.String(50), unique=True, nullable=False)
-    contraseña = db.Column(db.String(100), nullable=False)
+    contraseña_hash = db.Column(db.String(100), nullable=False)
     mail = db.Column(db.String(100), unique=True, nullable=False)
+
+    def set_password(self, password):
+        self.contraseña_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.contraseña_hash, password)
 
 class UsuarioSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nombreDeUsuario', 'contraseña', 'mail')
+        fields = ('id', 'nombreDeUsuario', 'mail')
 
 usuario_schema = UsuarioSchema()
 usuarios_schema = UsuarioSchema(many=True)
